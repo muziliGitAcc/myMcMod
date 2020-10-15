ff0546132023c55fcf8688f7e517dd36c31ca7d3
LC <1778267483@qq.com>
2020-10-15 18:49:13 +0800

    实体接受命令，并向控制台打印信息

--- mymod/src/main/java/my/computer/mod/myBlock/myComputerBlock.java
+++ mymod/src/main/java/my/computer/mod/myBlock/myComputerBlock.java
@@ -3,10 +3,7 @@ package my.computer.mod.myBlock;
 import my.computer.mod.Generic.BlockGeneric;
 import my.computer.mod.Generic.TileGeneric;
 import net.minecraft.tileentity.TileEntityType;
-import net.minecraft.util.Direction;
 import net.minecraftforge.fml.RegistryObject;
-
-import static net.minecraft.block.WallSignBlock.FACING;
 
 public class myComputerBlock extends BlockGeneric {
 
--- mymod/src/main/java/my/computer/mod/myTile/myComputerItle.java
+++ mymod/src/main/java/my/computer/mod/myTile/myComputerItle.java
@@ -1,10 +1,79 @@
 package my.computer.mod.myTile;
 
 import my.computer.mod.Generic.TileGeneric;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.DyeColor;
+import net.minecraft.item.DyeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.TranslationTextComponent;
+
+import javax.annotation.Nonnull;
 
 public class myComputerItle extends TileGeneric {
     public myComputerItle(TileEntityType<? extends TileGeneric> type) {
         super(type);
     }
+
+    @Nonnull
+    @Override
+    public ActionResultType onActivate(PlayerEntity player, Hand hand, BlockRayTraceResult hit )
+    {
+        TranslationTextComponent translationTextComponent = new TranslationTextComponent("message mymodmsg", 0);
+        player.sendStatusMessage(translationTextComponent, false);
+        return ActionResultType.SUCCESS;
+    }
+
+
+//    @Nonnull
+//    @Override
+//    public ActionResultType onActivate(PlayerEntity player, Hand hand, BlockRayTraceResult hit )
+//    {
+//        // Apply dye
+//        ItemStack currentItem = player.getHeldItem( hand );
+//        if( !currentItem.isEmpty() )
+//        {
+//            if( currentItem.getItem() instanceof DyeItem)
+//            {
+//                // Dye to change turtle colour
+//                if( !getWorld().isRemote )
+//                {
+//                    DyeColor dye = ((DyeItem) currentItem.getItem()).getDyeColor();
+//                    if( m_brain.getDyeColour() != dye )
+//                    {
+//                        m_brain.setDyeColour( dye );
+//                        if( !player.isCreative() )
+//                        {
+//                            currentItem.shrink( 1 );
+//                        }
+//                    }
+//                }
+//                return ActionResultType.SUCCESS;
+//            }
+//            else if( currentItem.getItem() == Items.WATER_BUCKET && m_brain.getColour() != -1 )
+//            {
+//                // Water to remove turtle colour
+//                if( !getWorld().isRemote )
+//                {
+//                    if( m_brain.getColour() != -1 )
+//                    {
+//                        m_brain.setColour( -1 );
+//                        if( !player.isCreative() )
+//                        {
+//                            player.setHeldItem( hand, new ItemStack( Items.BUCKET ) );
+//                            player.inventory.markDirty();
+//                        }
+//                    }
+//                }
+//                return ActionResultType.SUCCESS;
+//            }
+//        }
+//
+//        // Open GUI or whatever
+//        return super.onActivate( player, hand, hit );
+//    }
 }
