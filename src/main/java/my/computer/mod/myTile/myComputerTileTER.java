44ba0fddb36440e9628ccf74964db434a24d171c
CL <1778267483@qq.com>
2022-10-14 22:50:48 +0800

    --

--- src/main/java/my/computer/mod/myTile/myComputerTileTER.java
+++ src/main/java/my/computer/mod/myTile/myComputerTileTER.java
@@ -55,6 +55,7 @@ public class myComputerTileTER extends TileEntityRenderer<myComputerTile> {
         matrixStackIn.rotate( mc.getRenderManager().getCameraOrientation() );
         matrixStackIn.translate( -0.5f, -0.5f, -0.5f );
         //bufferIn.getBuffer(RenderTypeLookup.func_239220_a_(state, false)) 这个getBuffer中的参数 应该有限制 其他的有可能渲染不出来
+        //model.getRenderType(ROBOT_ENTITY_TEXTURE_02) 用这种方式获取的应该可以直接使用，其他方式应该都会有贴图异常的问题
         group.render(matrixStackIn, bufferIn.getBuffer(model.getRenderType(ROBOT_ENTITY_TEXTURE_02)), combinedLightIn, combinedOverlayIn, 1, 1, 1, 1);
         matrixStackIn.translate( -1.0f, -0.0f, -0.0f );
         matrixStackIn.pop();
